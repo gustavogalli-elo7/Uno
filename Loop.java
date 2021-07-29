018e7801c31b3ad0021aa50da9a0d878a1abb336
Ana Vitória <anavitoria_gouvea@usp.br>
2021-07-29 11:58:59 -0300

    validação de bots

--- Loop.java
+++ Loop.java
@@ -1,21 +1,44 @@
 import java.util.Scanner;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
 public class Loop {
+	Scanner scanner = new Scanner(System.in);
 
     private Jogo jogo;
+    
+    private void validaBots(int bots) {
+    	boolean adversarioValido = false;
+    	
+    	while(!adversarioValido) {
+    		adversarioValido = true;
+    		
+    		if(bots > 10 || bots < 1) {
+    			adversarioValido = false;
+    			System.out.println("Máximo de adversários é 9 e o mínimo é 1. Tente novamente.");
+    			bots = scanner.nextInt();
+    		}
+    	}	
+
+    }
+    
 
     public void iniciarLoop(){
 
-        Scanner scanner = new Scanner(System.in);
         this.jogo = new Jogo();
 
         System.out.println("Seja bem vindo(a)! Qual é o seu nome?");
         String nome = scanner.nextLine();
-
+        
+        nome = nome.substring(0, 1).toUpperCase() + nome.substring(1);
+        
         this.adicionaJogador(nome);
 
         System.out.println("Quantos adversários você quer, " + nome + "?");
         int bots = scanner.nextInt();
+        
+        this.validaBots(bots);
 
         this.adicionaBots(bots);
 
