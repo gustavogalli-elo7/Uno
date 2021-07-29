d04949c2d770f144bad6d60016914ed49e3c855a
Caio Dantas <caio.dantas@elo7.com>
2021-07-29 14:48:52 -0300

    Adiciona validação para o nome do jogador

--- Loop.java
+++ Loop.java
@@ -1,29 +1,10 @@
 import java.util.Scanner;
-import java.util.*;
 import java.lang.*;
-import java.io.*;
 
 public class Loop {
 	Scanner scanner = new Scanner(System.in);
 
     private Jogo jogo;
-    
-    private int validaBots(int bots) {
-    	boolean adversarioValido = false;
-    	
-    	while(!adversarioValido) {
-    		adversarioValido = true;
-    		
-    		if(bots > 10 || bots < 1) {
-    			adversarioValido = false;
-    			System.out.println("Máximo de adversários é 9 e o mínimo é 1. Tente novamente.");
-    			bots = scanner.nextInt();
-    		}
-    	}
-    	return bots;
-
-    }
-
     
 
     public void iniciarLoop(){
@@ -31,10 +12,9 @@ public class Loop {
         this.jogo = new Jogo();
 
         System.out.println("Seja bem vindo(a)! Qual é o seu nome?");
-        String nome = scanner.nextLine();
-        
-        nome = nome.substring(0, 1).toUpperCase() + nome.substring(1);
-        
+
+        String nome = this.validaNome(scanner.nextLine());
+
         this.adicionaJogador(nome);
 
         System.out.println("Quantos adversários você quer, " + nome + "?");
@@ -55,6 +35,36 @@ public class Loop {
         for (int i = 0; i < quantidade; i++){
             this.adicionaJogador(("Bot " + (i+1)));
         }
+    }
+
+    private int validaBots(int bots) {
+        boolean adversarioValido = false;
+
+        while(!adversarioValido) {
+            adversarioValido = true;
+
+            if(bots > 10 || bots < 1) {
+                adversarioValido = false;
+                System.out.println("Máximo de adversários é 9 e o mínimo é 1. Tente novamente.");
+                bots = scanner.nextInt();
+            }
+        }
+        return bots;
+    }
+
+    private String validaNome(String nome){
+        boolean nomeValido = false;
+
+        while(!nomeValido){
+            nomeValido = true;
+
+            if(nome.strip().length() <= 1){
+                nomeValido = false;
+                System.out.println("Por favor digite um nome válido.");
+                nome = scanner.nextLine();
+            }
+        }
+        return nome.strip().substring(0, 1).toUpperCase() + nome.strip().substring(1);
     }
 
 }
