79a3478749bc95d9a27223d7de72fe9baf074499
Gustavo Galli <gustavo.galli@elo7.com>
2021-07-30 10:40:36 -0300

    Refatorando distribuirCartas() com sugar syntax

--- Jogo.java
+++ Jogo.java
@@ -27,9 +27,9 @@ public class Jogo {
 		
 		// Distribui 7 cartas pra cada jogador, uma por vez
         for(int i = 0; i < 7; i++) {
-	    	for(int j = 0; j < jogadores.size(); j++) {
-	    		jogadores.get(j).receberCarta(deck.comprarCarta());
-	    	}
+        	for(Jogador jogador: jogadores) {
+        		jogador.receberCarta(deck.comprarCarta());
+        	}
         }
         
         // Distribui a vira
